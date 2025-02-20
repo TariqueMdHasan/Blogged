f243b876c90f2de03665baad38d336fc188607f1
Md Tarique Hasan <TariqueMdHasan@gmail.com>
2025-02-21 01:04:24 +0530

    ll

--- src/App.js
+++ src/App.js
@@ -3,22 +3,12 @@ import './App.css';
 import Landing from './pages/Landing';
 import Navbar from './components/navbar';
 import Auth from './pages/Auth';
-// import Catagory from './components/catagory';
-// import Blog from './pages/Blog';
-// import Blog from './pages/Blog';
 import Blog from './pages/Blog';
 import BlogForm from './components/BlogForm';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    // <div>
-    //   <Navbar/>
-    //   <Landing/>
-    //   <Catagory />
-    //   <Auth/>
-    //   <Blog />
-    // </div>
     <Router>
       <Navbar />
       <Routes>
