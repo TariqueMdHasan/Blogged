75e379fc1fad1a043684f25dbd60b2b5b12b4b6c
Md Tarique Hasan <TariqueMdHasan@gmail.com>
2025-02-25 22:11:48 +0530

    dj

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import './Landing.css'
 
 function Landing() {
+  const navigate = useNavigate()
   return (
-    <div>Landing</div>
+    <div className='Landing'>
+      <h1>go to Register page</h1>
+      <button
+        onClick={()=>{
+          navigate('/auth')
+        }}
+      >Register/Login</button>
+    </div>
   )
 }
 
